3f2549ba07a02e4e5ca279b6b74f681ba0440c36
zhang wenjun <32065482+Coding-xinxiangwanji2017@users.noreply.github.com>
2019-03-01 11:22:57 +0800

    Update Verilog_Demo.v

--- 1_Hdl/Verilog/Verilog_Demo.v
+++ 1_Hdl/Verilog/Verilog_Demo.v
@@ -11,31 +11,35 @@
 //                        **        **
 //        *****************          *****************
 //------------------------------------------------------------------------------
-//Com     :  BiXing Tech
+//Com     :  Company
 //Text    :  Verilog_Demo.v
 //Per     :  zhang wenjun
 //E-mail  :  xinxiangwanji2013@163.com
 //Data    :  2016/09/27
-//Cust    :  Verilog use demo 
+//Cust    :  Verilog Using Demo 
 //Int     :  0.1
 //IntTime :  1. Initial, zhang wenjun, 2016/09/27
 //------------------------------------------------------------------------------
-'timescale 1ns/100ps
+`timescale 1ns/100ps
 
-module Verilog_Demo (
-    //----------------------------------
-    // Reset & Clock
-    //----------------------------------
-    input  wire                         CpSl_Clk_i                              , // Clock signal 100M
-    input  wire                         CpSl_Rst_iN                             , // Reset active low
-    input  wire                         CpSl_Clk_iP                             , // Clock Diff_P 200M
-    input  wire                         CpSl_Clk_iN                             , // Clock Diff_N 200M
-    
-    //---------------------------------- 
-    // Uart interface             
-    //---------------------------------- 
-    input  wire                         CpSl_RxData_i                           , // RxData from PC
-    output wire                         CpSl_Txdata_o                             // TxData to PC
+module Verilog_Demo 
+    #(
+        parameter PrSv_LanceNum_c       = 1,
+        parameter PrSv_LinkNum_c        = 1 
+    )(
+        //------------------------------
+        // Reset & Clock
+        //------------------------------
+        input                           CpSl_Clk_i                              , // Clock signal 100M
+        input                           CpSl_Rst_iN                             , // Reset active low
+        input                           CpSl_Clk_iP                             , // Clock Diff_P 200M
+        input                           CpSl_Clk_iN                             , // Clock Diff_N 200M
+        
+        //------------------------------
+        // Uart interface             
+        //------------------------------
+        input                           CpSl_RxData_i                           , // RxData from PC
+        output                          CpSl_Txdata_o                             // TxData to PC
     );
 
     //----------------------------------
@@ -55,6 +59,7 @@ module Verilog_Demo (
     reg             PrSv_Cnt_r;
     reg  [2 : 0]    PrSv_Name_r;
     reg  [3 : 0]    PrSv_State_r;
+    reg             PrSl_TxData_r;
     
     //memory
     reg  [3 : 0]    PrSv__m [2 : 0];
@@ -82,26 +87,60 @@ module Verilog_Demo (
         else
             case (PrSv_State_r)
                 4h'0 : begin 
-                    
+                    if (PrSv_Name_r == 3) begin 
+                        PrSv_State_r <= 4h'1; 
+                    end
                 end 
                 
                 4h'1 : begin 
-                    
+                    PrSv_State_r <= 4h'0;
                 end   
-                    
-                
+
                 default : PrSv_State_r <= 4h'0;
             endcase
     end 
-
     
+    //----------------------------------
+    // Repeat_Demo
+    //----------------------------------
+    parameter size = 8;
+    parameter longsize = 16;
+    reg [size : 1] opa;
+    reg [size : 1] opb;
+    reg [longsize:1] result;
+    begin : mult
+        reg [longsize:1] shift_opa, shift_opb£»
+        shift_opa = opa;
+        shift_opb = opb;
+        result = 0;
+        repeat (size) begin
+            if(shift_opb[1])
+                result = result + shift_opa;
+                shift_opa = shift_opa << 1;
+                shift_opb = shift_opb >> 1;
+        end
+    end
+    
+    
+    //----------------------------------
+    // While_Demo
+    //----------------------------------
+    begin : count1s
+    reg  [7 : 0] tempreg;
+    count = 0;
+    tempreg = rega;
+    while(tempreg) begin
+        if(tempreg[0])
+            count = count + 1;
+            tempreg = tempreg >> 1;
+        end
+    end 
     
     
     //--------------------------------------------------------------------------
     // Out_Data
     //--------------------------------------------------------------------------
-    assign CpSl_Txdata_o = 1b'1;
-    
+    assign CpSl_Txdata_o = PrSl_TxData_r;
     
     
     //--------------------------------------------------------------------------
